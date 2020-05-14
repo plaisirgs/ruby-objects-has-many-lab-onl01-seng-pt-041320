7c5508ab221da33aa4e5926867a9e8c171bd9752
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-14 05:48:41 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,22 @@
+require 'pry'
 class Artist
+  attr_accessor :name, :songs
   
-
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs
+  end
+   
+  def add_song_by_name
+  
+  end
+    
+  def self.song_count
+    
+  end
+    
 end
--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,9 @@
 class Author
   
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
   
 end
